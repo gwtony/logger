3f93236986abe8e81eb70d9885bf5db7f209a513
guanwei <guanwei@mininglamp.com>
2019-07-25 17:03:59 +0800

    add sugar

--- logger.go
+++ logger.go
@@ -16,37 +16,21 @@ import (
 type Log struct {
 }
 
-// Configuration for logging
 type Config struct {
-	// EncodeLogsAsJson makes the log framework log JSON
 	EncodeLogsAsJson bool
-	// FileLoggingEnabled makes the framework log to a file
-	// the fields below can be skipped if this value is false!
 	FileLoggingEnabled bool
-	// Directory to log to to when filelogging is enabled
 	Directory string
-	// Filename is the name of the logfile which will be placed inside the directory
 	Filename string
-	// MaxSize the max size in MB of the logfile before it's rolled
 	MaxSize int
-	// MaxBackups the max number of rolled files to keep
 	MaxBackups int
-	// MaxAge the max age in days to keep a logfile
 	MaxAge int
-	// StackStrace make debug log stack
 	StackStrace bool
-	// LogLevel log level
+
 	LogLevel zapcore.Level
 }
 
-// How to log, by example:
-// logger.Info("Importing new file, zap.String("source", filename), zap.Int("size", 1024))
-// To log a stacktrace:
-// logger.Error("It went wrong, zap.Stack())
-
-// DefaultZapLogger is the default logger instance that should be used to log
-// It's assigned a default value here for tests (which do not call log.Configure())
-var DefaultZapLogger = newZapLogger(false, os.Stdout)
+var DefaultZapLogger *zap.Logger
+var DefaultZapSugaredLogger *zap.SugaredLogger
 var DefaultLoggerConfig Config
 
 func Bool(name string, value bool) zapcore.Field {
@@ -69,15 +53,10 @@ func Duration(name string, value time.Duration) zapcore.Field {
 	return zap.Duration(name, value)
 }
 
-func Err(err error) zapcore.Field {
+func Error(err error) zapcore.Field {
 	return zap.Error(err)
 }
 
-// Debug Log a message at the debug level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
 func (l *Log) Debug(msg string, fields ...zapcore.Field) {
 	if DefaultLoggerConfig.StackStrace {
 		fields = append(fields, Stack())
@@ -87,49 +66,41 @@ func (l *Log) Debug(msg string, fields ...zapcore.Field) {
 	}
 }
 
-// Info log a message at the info level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
+// *f prints "msg": "xxxx"
+// * prints "msg": "xxx", "key": "value"
 func (l *Log) Info(msg string, fields ...zapcore.Field) {
 	DefaultZapLogger.Info(msg, fields...)
 }
+func (l *Log) Infof(msg string, fields ...interface{}) {
+	DefaultZapSugaredLogger.Infof(msg, fields...)
+}
 
-// Warn log a message at the warn level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
 func (l *Log) Warn(msg string, fields ...zapcore.Field) {
 	DefaultZapLogger.Warn(msg, fields...)
 }
+func (l *Log) Warnf(msg string, fields ...interface{}) {
+	DefaultZapSugaredLogger.Warnf(msg, fields...)
+}
 
-// Error Log a message at the error level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
 func (l *Log) Error(msg string, fields ...zapcore.Field) {
 	DefaultZapLogger.Error(msg, fields...)
 }
+func (l *Log) Errorf(msg string, fields ...interface{}) {
+	DefaultZapSugaredLogger.Errorf(msg, fields...)
+}
 
-// Panic Log a message at the Panic level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
 func (l *Log) Panic(msg string, fields ...zapcore.Field) {
 	DefaultZapLogger.Panic(msg, fields...)
 }
+func (l *Log) Panicf(msg string, fields ...interface{}) {
+	DefaultZapSugaredLogger.Panicf(msg, fields...)
+}
 
-// Fatal Log a message at the fatal level. Messages include any context that's
-// accumulated on the logger, as well as any fields added at the log site.
-//
-// Use zap.String(key, value), zap.Int(key, value) to log fields. These fields
-// will be marshalled as JSON in the logfile and key value pairs in the console!
 func (l *Log) Fatal(msg string, fields ...zapcore.Field) {
 	DefaultZapLogger.Fatal(msg, fields...)
+}
+func (l *Log) Fatalf(msg string, fields ...interface{}) {
+	DefaultZapSugaredLogger.Fatalf(msg, fields...)
 }
 
 func Stack() zapcore.Field {
@@ -141,35 +112,6 @@ func Stack() zapcore.Field {
 	return zap.String("stacktrace", src)
 }
 
-//// AtLevel logs the message at a specific log level
-//func AtLevel(level zapcore.Level, msg string, fields ...zapcore.Field) {
-//	switch level {
-//	case zapcore.DebugLevel:
-//		Debug(msg, fields...)
-//	case zapcore.PanicLevel:
-//		Panic(msg, fields...)
-//	case zapcore.ErrorLevel:
-//		Error(msg, fields...)
-//	case zapcore.WarnLevel:
-//		Warn(msg, fields...)
-//	case zapcore.InfoLevel:
-//		Info(msg, fields...)
-//	case zapcore.FatalLevel:
-//		Fatal(msg, fields...)
-//	default:
-//		Warn("Logging at unkown level", zap.Any("level", level))
-//		Warn(msg, fields...)
-//	}
-//}
-
-// Configure sets up the logging framework
-//
-// In production, the container logs will be collected and file logging should be disabled. However,
-// during development it's nicer to see logs as text and optionally write to a file when debugging
-// problems in the containerized pipeline
-//
-// The output log file will be located at /var/log/auth-service/auth-service.log and
-// will be rolled when it reaches 20MB with a maximum of 1 backup.
 func Configure(config Config) {
 	writers := []zapcore.WriteSyncer{os.Stdout}
 	if config.FileLoggingEnabled {
@@ -178,14 +120,7 @@ func Configure(config Config) {
 
 	DefaultZapLogger = newZapLogger(config.EncodeLogsAsJson, zapcore.NewMultiWriteSyncer(writers...))
 	zap.RedirectStdLog(DefaultZapLogger)
-	//Info("logging configured",
-	//	zap.Bool("fileLogging", config.FileLoggingEnabled),
-	//	zap.Bool("jsonLogOutput", config.EncodeLogsAsJson),
-	//	zap.String("logDirectory", config.Directory),
-	//	zap.String("fileName", config.Filename),
-	//	zap.Int("maxSizeMB", config.MaxSize),
-	//	zap.Int("maxBackups", config.MaxBackups),
-	//	zap.Int("maxAgeInDays", config.MaxAge))
+	DefaultZapSugaredLogger = DefaultZapLogger.Sugar()
 	DefaultLoggerConfig = config
 }
 
@@ -237,21 +172,21 @@ func newRollingFile(config Config) zapcore.WriteSyncer {
 		MaxSize:    config.MaxSize,    //megabytes
 		MaxAge:     config.MaxAge,     //days
 		MaxBackups: config.MaxBackups, //files
+		LocalTime: true,
 	})
 }
 
 func newZapLogger(encodeAsJSON bool, output zapcore.WriteSyncer) *zap.Logger {
 	encCfg := zapcore.EncoderConfig{
-		TimeKey:        "timestamp",
+		TimeKey:        "time",
 		LevelKey:       "level",
 		NameKey:        "logger",
 		CallerKey:      "caller",
 		MessageKey:     "msg",
 		StacktraceKey:  "stacktrace",
 		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		//EncodeTime:     zapcore.EpochNanosTimeEncoder,
-		//EncodeTime:     zapcore.ISO8601TimeEncoder,
-		EncodeTime:     zapcore.EpochMillisTimeEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder, //default print time in "2019-07-25T16:49:46.037+0800"
+		//EncodeTime:     zapcore.EpochMillisTimeEncoder,
 		EncodeDuration: zapcore.NanosDurationEncoder,
 	}
 
